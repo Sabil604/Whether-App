fb3d7d91519d22df5cf68a23876a3634c9b941f0
Sabil604 <sksabilmujawar@gmail.com>
2024-10-25 12:29:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace my_Work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Enter the City:");
+            string city = Console.ReadLine();
+
+            Console.WriteLine($"Fetching data of the {city}...");
+
+            Console.WriteLine("Current temprature: 25° C");
+        }
+       
+    }
+}
